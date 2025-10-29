fc2429e1939fd2f93658889cd4f2f928dd190e33
kibria900 <kibriahassan3500@gmail.com>
2025-10-29 15:36:16 +0600

    update api

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Card from '../Components/Card'
+// Docs.jsx
+import React, { useEffect, useState } from 'react';
+import Card from '../Components/Card';
 
 const Docs = () => {
-  return (
- <>
- 
- <Card/>
- </>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default Docs
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
+      {products.map((product) => (
+        <Card key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Docs;
